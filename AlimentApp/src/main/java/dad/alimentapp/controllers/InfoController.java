27902267e1337e13ada66a62a375b6488d501966
David18888 <diazbarriosdavid@gmail.com>
2021-01-25 13:25:33 +0000

    Creación de vistas información y mis datos

---
+++ AlimentApp/src/main/java/dad/alimentapp/controllers/InfoController.java
@@ -0,0 +1,50 @@
+package dad.alimentapp.controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
+public class InfoController implements Initializable{
+	
+
+    @FXML
+    private BorderPane view;
+
+    @FXML
+    private ImageView adviceImage;
+
+    @FXML
+    private GridPane dietaPane;
+
+    @FXML
+    private Label dietaLabel;
+	
+	public InfoController() throws IOException  {
+		
+		
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/InfoView.fxml"));
+		loader.setController(this);
+		loader.load();
+		
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+public BorderPane getView() {
+	return view;
+}	
+	
+
+}
