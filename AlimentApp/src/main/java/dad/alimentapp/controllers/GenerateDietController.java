c6cd1218cccbc97f3179ef377c2886ddc633ad7c
DanielPlasenciaOran2DAMA <1bacdanielplasencia@gmail.com>
2021-03-01 16:11:42 +0000

    Generar Dietas y Menu
    Interfaz final de Generar dietas y menú

--- AlimentApp/src/main/java/dad/alimentapp/controllers/GenerateDietController.java
+++ AlimentApp/src/main/java/dad/alimentapp/controllers/GenerateDietController.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import org.controlsfx.control.CheckComboBox;
-
-import dad.alimentapp.models.Weekday;
+import dad.alimentapp.models.Product;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -16,103 +14,66 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.Slider;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GenerateDietController implements Initializable {
 
 	@FXML
-	private BorderPane view;
+	private VBox view;
 
 	@FXML
-	private TextField nameText;
+	private GridPane dietGrid;
 
 	@FXML
-	private Button generateButton;
+	private Label kcalLabel;
 
 	@FXML
-	private Button cancelButton;
+	private Label hydratsLabel;
 
 	@FXML
-	private RadioButton defaultRadio;
+	private Label proteinLabel;
 
 	@FXML
-	private ToggleGroup configure;
+	private Label fatsLabel;
 
 	@FXML
-	private RadioButton configureRadio;
-
-	@FXML
-	private VBox dietTypeVBox;
-
-	@FXML
-	private HBox dietTypeBox;
+	private Label fiberLabel;
 
 	@FXML
 	private ComboBox<String> dietTypeCombo;
 
 	@FXML
-	private Label descriptionLabel;
+	private ListView<Product> breakfastListView;
 
 	@FXML
-	private VBox personalizedVBox;
+	private ListView<Product> midMorningListView;
 
 	@FXML
-	private GridPane personalizedPane;
+	private ListView<Product> lunchListView;
 
 	@FXML
-	private HBox kcalBox;
+	private ListView<Product> snackListView;
 
 	@FXML
-	private Slider kcalSlider;
+	private ListView<Product> dinnerListView;
 
 	@FXML
-	private Label recomend1Label;
+	private TextField dietNameText;
 
 	@FXML
-	private Label recomend2Label;
+	private Button saveButton;
 
 	@FXML
-	private Label numberCalLabel;
+	private Button cancelButton;
 
-	@FXML
-	private Slider hydrartsSlider;
+	private String diet1 = "DIETA HIPERCALÓRICA", diet2 = "DIETA HIPOCALÓRICA", diet3 = "DIETA HIPERPROTEICA",
+			diet4 = "DIETA VEGANA", diet5 = "DIETA ESTÁNDAR";
 
-	@FXML
-	private Label numberHydrLabel;
-
-	@FXML
-	private Slider proteinSlider;
-
-	@FXML
-	private Label recomend3Label;
-
-	@FXML
-	private Label numberProtLabel;
-
-	@FXML
-	private Label numberFatsLabel;
-
-	@FXML
-	private Label recomend4Label;
-
-	@FXML
-	private Slider fatsSlider;
-
-	@FXML
-	private CheckComboBox<Weekday> daysCheckCombo;
-
-	private String item1 = "DIETA HIPERCALÓRICA", item2 = "DIETA HIPOCALÓRICA", item3 = "DIETA HIPERPROTEICA",
-			item4 = "DIETA VEGANA", item5 = "DIETA ESTÁNDAR";
-
-	ObservableList<String> dietsList = FXCollections.observableArrayList(item1, item2, item3, item4, item5);
+	ObservableList<String> dietsList = FXCollections.observableArrayList(diet1, diet2, diet3, diet4, diet5);
 
 	public GenerateDietController() throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/GenerateDietView.fxml"));
@@ -122,203 +83,30 @@ public class GenerateDietController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		dietTypeCombo.setValue(item1);
+		dietTypeCombo.setValue(diet1);
 		dietTypeCombo.setItems(dietsList);
-		dietTypeCombo.setValue(item2);
+		dietTypeCombo.setValue(diet2);
 		dietTypeCombo.setItems(dietsList);
-		dietTypeCombo.setValue(item3);
+		dietTypeCombo.setValue(diet3);
 		dietTypeCombo.setItems(dietsList);
-		dietTypeCombo.setValue(item4);
+		dietTypeCombo.setValue(diet4);
 		dietTypeCombo.setItems(dietsList);
-		dietTypeCombo.setValue(item5);
+		dietTypeCombo.setValue(diet5);
 		dietTypeCombo.setItems(dietsList);
-
-		ObservableList<Weekday> dayList = FXCollections.observableArrayList();
-		for (int i = 0; i <= 0; i++) {
-			dayList.addAll(Weekday.values());
-		}
-		daysCheckCombo.getItems().addAll(dayList);
-		daysCheckCombo.getCheckModel().check(0);
-		daysCheckCombo.getCheckModel().check(2);
-		daysCheckCombo.getCheckModel().check(4);
-		daysCheckCombo.getCheckModel().check(6);
-
-		kcalSlider.setMax(3000);
-		kcalSlider.setMin(0);
-		kcalSlider.setValue(2000);
-		numberCalLabel.setText(String.valueOf(kcalSlider.getValue()));
-
-		kcalSlider.valueProperty().addListener((o, ov, nv) -> {
-			numberCalLabel.setText(String.format("%.0f", nv));
-		});
-
-		hydrartsSlider.setMax(200);
-		hydrartsSlider.setMin(0);
-		hydrartsSlider.setValue(150);
-		numberHydrLabel.setText(String.valueOf(hydrartsSlider.getValue()));
-
-		hydrartsSlider.valueProperty().addListener((o, ov, nv) -> {
-			numberHydrLabel.setText(String.format("%.0f", nv));
-		});
-
-		proteinSlider.setMax(100);
-		proteinSlider.setMin(0);
-		proteinSlider.setValue(40);
-		numberProtLabel.setText(String.valueOf(proteinSlider.getValue()));
-
-		proteinSlider.valueProperty().addListener((o, ov, nv) -> {
-			numberProtLabel.setText(String.format("%.0f", nv));
-		});
-
-		fatsSlider.setMax(100);
-		fatsSlider.setMin(0);
-		fatsSlider.setValue(55);
-		numberFatsLabel.setText(String.valueOf(fatsSlider.getValue()));
-
-		fatsSlider.valueProperty().addListener((o, ov, nv) -> {
-			numberFatsLabel.setText(String.format("%.0f", nv));
-		});
-
-		dietTypeBox.setDisable(true);
-		personalizedPane.setDisable(true);
-
-		defaultRadio.setSelected(true);
-
-		descriptionLabel.setText("Una dieta equilibrada, basada en los valores orientativos de consumo \n"
-				+ "de nutrientes para cada persona.");
-
-		configureRadio.selectedProperty().addListener((o, ov, nv) -> {
-			if (configureRadio.isSelected()) {
-				dietTypeBox.setDisable(false);
-				personalizedPane.setDisable(false);
-				dietTypeCombo.getSelectionModel().clearSelection();
-				kcalSlider.setValue(0);
-				hydrartsSlider.setValue(0);
-				proteinSlider.setValue(0);
-				fatsSlider.setValue(0);
-				descriptionLabel.setText(" ");
-				daysCheckCombo.getCheckModel().clearChecks();
-			} else if (defaultRadio.isSelected()) {
-				dietTypeBox.setDisable(true);
-				personalizedPane.setDisable(true);
-				dietTypeCombo.getSelectionModel().select(4);
-				kcalSlider.setValue(2000);
-				hydrartsSlider.setValue(150);
-				proteinSlider.setValue(40);
-				fatsSlider.setValue(55);
-				daysCheckCombo.getCheckModel().check(0);
-				daysCheckCombo.getCheckModel().check(2);
-				daysCheckCombo.getCheckModel().check(4);
-				daysCheckCombo.getCheckModel().check(6);
-				descriptionLabel.setText("Una dieta equilibrada, basada en los valores orientativos de consumo \n"
-						+ "de nutrientes para cada persona.");
-			}
-		});
-
-		dietTypeCombo.getSelectionModel().selectedIndexProperty().addListener((o, ov, nv) -> {
-			showDescription();
-		});
-
-		dietTypeCombo.getSelectionModel().selectedIndexProperty().addListener((o, ov, nv) -> {
-			recommendsLabel();
-		});
-	}
-
-
-	private void showDescription() {
-		try {
-			if (dietTypeCombo.getSelectionModel().getSelectedIndex() == 0) {
-				descriptionLabel.setText("Dieta destinada a la ganancia de peso, para  personas cuyo IMC \n"
-						+ "es inferior a 18 o simplemente para personas que se ven demasiado \n"
-						+ "delgadas y quisieran subir" + "de peso de manera equilibrada y \ncomiendo sano");
-			} else if (dietTypeCombo.getSelectionModel().getSelectedIndex() == 1) {
-				descriptionLabel.setText("Dieta destinada a la pérdida de peso , recomendada para personas cuyo \n"
-						+ "IMC es superior a 25, consistente en una reducción en el número de \n" + "kcal a consumir");
-			} else if (dietTypeCombo.getSelectionModel().getSelectedIndex() == 2) {
-				descriptionLabel.setText("Dieta destinada al aumento de la masa muscular a través de un mayor \n"
-						+ "consumo de proteínas y una reducción en la ingesta de hidratos de carbono.\n"
-						+ "Ideal para personas que realizan actividad física intensa y para personas que \n"
-						+ "quieran aumentar su masa muscular.");
-			} else if (dietTypeCombo.getSelectionModel().getSelectedIndex() == 3) {
-				descriptionLabel.setText("Dieta destinada a personas que hayan renunciado a tomar todo tipo \n"
-						+ "de alimentos de origen animal, por lo tanto, los alimentos de esta dieta deben \n"
-						+ "ser todos de origen vegetal, buscando un equilibrio que logre aportar todos los \n"
-						+ "nutrientes necesarios para el día a día.");
-			} else if (dietTypeCombo.getSelectionModel().getSelectedIndex() == 4) {
-				descriptionLabel.setText("Una dieta equilibrada, basada en los valores orientativos de consumo \n"
-						+ "de nutrientes para cada persona.");
-			}
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-	}
-
-	private void recommendsLabel() {
-		try {
-			if (dietTypeCombo.getSelectionModel().getSelectedIndex() == 0) {
-				recomend1Label.setText("2300");
-				recomend3Label.setText("80");
-				recomend2Label.setText("185");
-				recomend4Label.setText("65");
-				kcalSlider.setValue(2300);
-				hydrartsSlider.setValue(185);
-				proteinSlider.setValue(80);
-				fatsSlider.setValue(65);
-			} else if (dietTypeCombo.getSelectionModel().getSelectedIndex() == 1) {
-				recomend1Label.setText("1800");
-				recomend3Label.setText("30");
-				recomend2Label.setText("120");
-				recomend4Label.setText("40");
-				kcalSlider.setValue(1800);
-				hydrartsSlider.setValue(120);
-				proteinSlider.setValue(30);
-				fatsSlider.setValue(40);
-			} else if (dietTypeCombo.getSelectionModel().getSelectedIndex() == 2) {
-				recomend1Label.setText("1900");
-				recomend3Label.setText("90");
-				recomend2Label.setText("140");
-				recomend4Label.setText("52");
-				kcalSlider.setValue(1900);
-				hydrartsSlider.setValue(140);
-				proteinSlider.setValue(90);
-				fatsSlider.setValue(52);
-			} else if (dietTypeCombo.getSelectionModel().getSelectedIndex() == 3) {
-				recomend1Label.setText("2350");
-				recomend3Label.setText("19");
-				recomend2Label.setText("137");
-				recomend4Label.setText("40");
-				kcalSlider.setValue(2350);
-				hydrartsSlider.setValue(137);
-				proteinSlider.setValue(19);
-				fatsSlider.setValue(40);
-			} else if (dietTypeCombo.getSelectionModel().getSelectedIndex() == 4) {
-				recomend1Label.setText("2000");
-				recomend3Label.setText("40");
-				recomend2Label.setText("150");
-				recomend4Label.setText("55");
-				kcalSlider.setValue(2000);
-				hydrartsSlider.setValue(150);
-				proteinSlider.setValue(40);
-				fatsSlider.setValue(55);
-			}
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-
 	}
 
 	@FXML
-	void onCancelButtonAction(ActionEvent event) {
+	void onCancelDietButtonAction(ActionEvent event) {
 		Stage stage = (Stage) this.cancelButton.getScene().getWindow();
 		stage.close();
 	}
 
 	@FXML
-	void onGenerateButtonAction(ActionEvent event) {
-		
+	void onSaveDietButtonAction(ActionEvent event) {
+
 	}
 
-	public BorderPane getView() {
+	public VBox getView() {
 		return view;
 	}
 
--- AlimentApp/src/main/java/dad/alimentapp/controllers/GenerateMenuController.java
+++ AlimentApp/src/main/java/dad/alimentapp/controllers/GenerateMenuController.java
@@ -4,11 +4,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import org.controlsfx.control.CheckComboBox;
-
+import dad.alimentapp.models.Menu;
 import dad.alimentapp.models.Product;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import dad.alimentapp.service.MenuService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,15 +16,21 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+/**
+ * Esta pestaña creara un menu por defecto, seleccionandolo en un combobox. Se
+ * visualizará en una lista los productos y en unos label las cantidades de
+ * kilocalorias, hidratos, proteinas, grasas y fibras
+ * 
+ * @author Daniel
+ *
+ */
 public class GenerateMenuController implements Initializable {
-
 	@FXML
 	private VBox view;
 
@@ -46,25 +50,28 @@ public class GenerateMenuController implements Initializable {
 	private Label fatsLabel;
 
 	@FXML
-	private Label productsLabel;
-
-	@FXML
 	private Label fiberLabel;
 
 	@FXML
 	private ComboBox<String> menuTypeCombo;
 
 	@FXML
+	private ListView<Product> breakfastListView;
+
+	@FXML
+	private ListView<Product> midMorningListView;
+
+	@FXML
+	private ListView<Product> lunchListView;
+
+	@FXML
+	private ListView<Product> snackListView;
+
+	@FXML
+	private ListView<Product> dinnerListView;
+
+	@FXML
 	private TextField menuNameText;
-
-	@FXML
-	private RadioButton defaultRadio;
-
-	@FXML
-	private ToggleGroup choose;
-
-	@FXML
-	private RadioButton configureRadio;
 
 	@FXML
 	private Button saveButton;
@@ -72,12 +79,19 @@ public class GenerateMenuController implements Initializable {
 	@FXML
 	private Button cancelButton;
 
-	private String menu1 = "MENU PROTEICO", menu2 = "MENU VEGANO", menu3 = "MENU DE PESCADO",
-			menu4 = "MENU VEGETARIANO", menu5 = "MENU ESTÁNDAR";
+	private String menu1 = "MENU PROTEICO", menu2 = "MENU VEGANO", menu3 = "MENU FÍBRICO", menu4 = "MENU VEGETARIANO",
+			menu5 = "MENU ESTÁNDAR";
+
+	private String product1B = "LECHE CON MIEL", product2B = "TOSTADAS CON JAMON", product3B = "YOGUR CON FRUTAS",
+			product4B = "TOSTADAS CON QUESO DE UNTAR", product5B = "SANDWICH PAVO",
+			product6B = "REQUESÓN CON MIEL Y NUECES", product1MM = "TORTITAS DE ARROZ", product2MM = "YOGUR NATURAL",
+			product3MM = "ZUMO DE NARANJA", product4MM = "FRUTOS SECOS", product1L = "ARROZ",
+			product2L = "PECHUGA DE PAVO", product3L = "ENSALADA", product4L = "POLLO A LA PLANCHA", product5L = "ATUN",
+			product1S = "YOGUR CON ALMENDRAS", product2S = "HUEVOS DUROS", product3S = "TOSTADAS CON AGUACATE",
+			product4S = "BROCHETAS DE FRUTAS", product1D = "PECHUGA DE POLLA AL LIMON", product2D = "GUISANTES",
+			product3D = "CALAMARES", product4D = "SOPA DE PESCADO", product5D = "SANDWICH DE TORTILLA FRANCESA";
 
 	ObservableList<String> menuList = FXCollections.observableArrayList(menu1, menu2, menu3, menu4, menu5);
-
-	private Product products;
 
 	public GenerateMenuController() throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/GenerateMenuView.fxml"));
@@ -99,26 +113,20 @@ public class GenerateMenuController implements Initializable {
 		menuTypeCombo.setValue(menu5);
 		menuTypeCombo.setItems(menuList);
 
-		menuGrid.setDisable(true);
-
-		defaultRadio.setSelected(true);
-
-		configureRadio.selectedProperty().addListener((o, ov, nv) -> {
-			if (configureRadio.isSelected()) {
-				menuGrid.setDisable(false);
-				menuTypeCombo.getSelectionModel().clearSelection();
-			} else if (defaultRadio.isSelected()) {
-				menuGrid.setDisable(true);
-				menuTypeCombo.getSelectionModel().select(4);
-			}
-		});
+		menuTypeCombo.getSelectionModel().selectFirst();
+		System.out.println(menuTypeCombo.getSelectionModel().getSelectedIndex());
 
 		menuTypeCombo.selectionModelProperty().addListener((o, ov, nv) -> {
-			showDescriptionMenus();
-			sumMenus();
+			listProductList();
 		});
 	}
 
+	/**
+	 * Esta función hara que cierre la pestaña abierta, si no desea realizar ninguna
+	 * operación en ella.
+	 * 
+	 * @param event
+	 */
 	@FXML
 	void onCancelMenuButtonAction(ActionEvent event) {
 		Stage stage = (Stage) this.cancelButton.getScene().getWindow();
@@ -127,56 +135,44 @@ public class GenerateMenuController implements Initializable {
 
 	@FXML
 	void onSaveMenuButtonAction(ActionEvent event) {
-
+		/*Menu menuName = new Menu(menuNameText.getText());
+		MenuService.insertMenu(menuName);*/
 	}
 
-	private void showDescriptionMenus() {
-		try {
-			if (menuTypeCombo.getSelectionModel().getSelectedIndex() == 0) {
-				productsLabel.setText("Sandwich de pavo (desayuno), Tortitas de arroz (media-mañana), \n"
-						+ "Pollo a la plancha (Almuerzo), Huevos duros (merienda), \n"
-						+ "Pechuga de pollo al limón (cena)");
-				kcalLabel.setText("739");
-				hydratsLabel.setText("53.04");
-				proteinLabel.setText("72.96");
-				fatsLabel.setText("19.05");
-				fiberLabel.setText("4");
-			} else if (menuTypeCombo.getSelectionModel().getSelectedIndex() == 1) {
-				productsLabel.setText("Tostadas con jamón y yogur con frutas. ");
-				kcalLabel.setText("276");
-				hydratsLabel.setText("30");
-				proteinLabel.setText("12.4");
-				fatsLabel.setText("8");
-				fiberLabel.setText("2");
-			} else if (menuTypeCombo.getSelectionModel().getSelectedIndex() == 2) {
-				productsLabel.setText("Tortitas de arroz y zumo de naranja. ");
-				kcalLabel.setText("224");
-				hydratsLabel.setText("48.7");
-				proteinLabel.setText("4.3");
-				fatsLabel.setText("1.5");
-				fiberLabel.setText("1");
-			} else if (menuTypeCombo.getSelectionModel().getSelectedIndex() == 3) {
-				productsLabel.setText("Frutos secos y yogur natural. ");
-				kcalLabel.setText("563");
-				hydratsLabel.setText("11.04");
-				proteinLabel.setText("9.25");
-				fatsLabel.setText("10.55");
-				fiberLabel.setText("2");
-			} else if (menuTypeCombo.getSelectionModel().getSelectedIndex() == 4) {
-				productsLabel.setText("Pechuga de pavo y arroz. ");
-				kcalLabel.setText("308");
-				hydratsLabel.setText("48.2");
-				proteinLabel.setText("21.9");
-				fatsLabel.setText("2");
-				fiberLabel.setText("1.1");
-			}
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-	}
-
-	private void sumMenus() {
-
+	private void listProductList() {
+		/*if (menuTypeCombo.getSelectionModel().isSelected(0)) {
+			breakfastListView.getItems().add(product4B);
+			breakfastListView.getItems().add(product5B);
+			breakfastListView.getItems().add(product6B);
+			midMorningListView.getItems().add(product1MM);
+			midMorningListView.getItems().add(product2MM);
+			midMorningListView.getItems().add(product4MM);
+			lunchListView.getItems().add(product5L);
+			lunchListView.getItems().add(product2L);
+			lunchListView.getItems().add(product4L);
+			snackListView.getItems().add(product1S);
+			snackListView.getItems().add(product2S);
+			snackListView.getItems().add(product3S);
+			dinnerListView.getItems().add(product1D);
+			dinnerListView.getItems().add(product4D);
+			dinnerListView.getItems().add(product5D);
+		} else if (menuTypeCombo.getSelectionModel().isSelected(4)) {
+			breakfastListView.getItems().add(product1B);
+			breakfastListView.getItems().add(product2B);
+			breakfastListView.getItems().add(product3B);
+			midMorningListView.getItems().add(product1MM);
+			midMorningListView.getItems().add(product2MM);
+			midMorningListView.getItems().add(product3MM);
+			lunchListView.getItems().add(product1L);
+			lunchListView.getItems().add(product2L);
+			lunchListView.getItems().add(product3L);
+			snackListView.getItems().add(product1S);
+			snackListView.getItems().add(product2S);
+			snackListView.getItems().add(product3S);
+			dinnerListView.getItems().add(product1D);
+			dinnerListView.getItems().add(product2D);
+			dinnerListView.getItems().add(product3D);
+		}*/
 	}
 
 	public VBox getView() {
